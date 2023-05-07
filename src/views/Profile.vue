4d8890b003612440e4af6801a14f132e348a8c1d
DeniVidan <denividan@gmail.com>
2023-05-07 11:33:59 +0200

    image compression

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -19,15 +19,23 @@
         <div class="profilepic">
           <img
             class="profilepic__image"
-            src="https://picsum.photos/170/170"
+            src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
             width="170"
             height="170"
-            alt="Profibild"
+            alt="Profile picture"
+            ref="profilePic"
           />
-          <div class="profilepic__content">
+          <div @click="openFileExplorer" class="profilepic__content">
             <span class="profilepic__icon"></span>
             <span class="profilepic__text">Edit Profile</span>
           </div>
+          <input
+            type="file"
+            accept="image/*"
+            @change="onChange"
+            ref="fileInput"
+            style="display: none"
+          />
         </div>
         <div class="name">
           {{ currentUser.firstname }} {{ currentUser.lastname }}
@@ -67,8 +75,9 @@
               name="password"
               placeholder="Enter your new password"
             />
-            <div class="button"><div class="" @click="editUserInfo()">submit</div></div>
-            
+            <div class="button">
+              <div class="" @click="editUserInfo()">submit</div>
+            </div>
           </div>
         </div>
       </div>
@@ -78,6 +87,8 @@
 
 <script>
 import { Auth, Service } from "../services/services";
+import imageCompression from "browser-image-compression";
+
 export default {
   name: "Profile",
   data() {
@@ -94,6 +105,7 @@ export default {
       isToggle: false,
       error: "",
       changePass: false,
+      base64compressed: null,
     };
   },
   methods: {
@@ -151,6 +163,44 @@ export default {
         console.log(error);
       }
     },
+
+    openFileExplorer() {
+      this.$refs.fileInput.click();
+    },
+    async onChange(event) {
+      try {
+        const file = event.target.files[0];
+
+        console.log("originalFile instanceof Blob", file instanceof Blob); // true
+        console.log(`originalFile size ${file.size / 1024 / 1024} MB`);
+
+        const options = {
+          maxSizeMB: 0.1,
+          maxWidthOrHeight: 480,
+          useWebWorker: true,
+        };
+
+        const compressedFile = await imageCompression(file, options);
+
+        console.log(
+          "compressedFile instanceof Blob",
+          compressedFile instanceof Blob
+        ); // true
+        console.log(
+          `compressedFile size ${compressedFile.size / 1024 / 1024} MB`
+        ); // smaller than maxSizeMB
+
+        const reader = new FileReader();
+        reader.readAsDataURL(compressedFile);
+        reader.onload = () => {
+          this.base64compressed = reader.result;
+          this.$refs.profilePic.setAttribute("src", this.base64compressed);
+          console.log("slika: ", this.base64compressed);
+        };
+      } catch (error) {
+        console.log("error compresion: ", error);
+      }
+    },
   },
   mounted() {},
 };
@@ -171,12 +221,6 @@ export default {
   margin-top: 70px;
 }
 
-
-
-
-
-
-
 .profilepic {
   position: relative;
   margin: auto;
@@ -192,13 +236,13 @@ export default {
 }
 
 .profilepic:hover .profilepic__image {
-  opacity: .5;
+  opacity: 0.5;
 }
 
 .profilepic__image {
   object-fit: cover;
   opacity: 1;
-  transition: opacity .2s ease-in-out;
+  transition: opacity 0.2s ease-in-out;
   cursor: pointer;
 }
 
@@ -214,7 +258,7 @@ export default {
   align-items: center;
   color: white;
   opacity: 0;
-  transition: opacity .2s ease-in-out;
+  transition: opacity 0.2s ease-in-out;
   cursor: pointer;
 }
 
@@ -227,7 +271,6 @@ export default {
   cursor: pointer;
 }
 
-
 .profilepic__text {
   text-transform: uppercase;
   font-size: 12px;
@@ -235,8 +278,6 @@ export default {
   text-align: center;
   cursor: pointer;
 }
-
-
 
 .name {
   color: white;
@@ -294,17 +335,13 @@ export default {
   justify-content: flex-end;
   margin-top: 20px;
   margin-bottom: 20px;
-  
-  
-  
-  
 }
 .button > div {
-background-color: #2e70eb;
-padding: 8px 25px;
-cursor: pointer;
-font-weight: bold;
-color: white;
-border-radius: 10px;
+  background-color: #2e70eb;
+  padding: 8px 25px;
+  cursor: pointer;
+  font-weight: bold;
+  color: white;
+  border-radius: 10px;
 }
 </style>
